152fde2e6fe2798f8dd37ab8a69db08cb7a451f6
snehakumar9 <snehakumar9@gmail.com>
2025-03-13 09:08:54 -0700

    Moved data frame info to Assignment 5

--- Function/forest_sequestration.R
+++ Function/forest_sequestration.R
@@ -1,3 +1,5 @@
+
+
 forest_sequestration <- function(tree_density=NULL, avg_diameter=NULL, avg_height=NULL,
                                       carbon_fraction = 0.5, bm_conversion_factor = 0.1, growth_rate = 0.03,tree_data=NULL){ 
   # Documentation:
@@ -14,31 +16,6 @@ forest_sequestration <- function(tree_density=NULL, avg_diameter=NULL, avg_heigh
   # > would switch out depending on your data, and found using allometric modeling)
   # growth_rate: annual growth rate (as a decimal)
   # > note: according to US Forest Service (2021 report), it stands on avg at 3%. (0.03 in decimal)
-  
-  # If raw tree data is provided by user, calculate the averages and density.
-  if (!is.null(tree_data)) {
-    avg_diameter <- mean(tree_data$diameter, na.rm = TRUE)
-    avg_height   <- mean(tree_data$height, na.rm = TRUE)
-  
-  
-  
-  
-  
-  # Combine tree dimensions into single variable for clarity / modularity
-  tree_dimensions <- avg_diameter * avg_height
-  
-  # Estimate biomass per tree based on combined tree dimensions
-  biomass_per_tree <- bm_conversion_factor * tree_dimensions
-  
-  # Total biomass per hectare (in tons)
-  total_biomass <- tree_density * biomass_per_tree
-  
-  # Annual forest carbon sequestration: (growth-related biomass increase) * (carbon fraction)
-  annual_sequestration <- total_biomass * growth_rate * carbon_fraction
-  
-  # Return both outputs as a list
-  return(list(annual_sequestration = annual_sequestration, total_biomass = total_biomass))
-
 
 # Example usage (given some values):
 result2 <- forest_sequestration(tree_density = 500, avg_diameter = 30, avg_height = 10)
