87178953274486fbf1a089aea134ec35857a9a51
bellaluvly <perezisabella11@gmail.com>
2025-03-13 09:05:42 -0700

    minor edits

--- R/forest_sequestration.R
+++ R/forest_sequestration.R
@@ -1,7 +1,9 @@
-forest_sequestration <- function(tree_density, avg_diameter, avg_height,
-                                      carbon_fraction = 0.5, bm_conversion_factor = 0.1, growth_rate = 0.03){ 
+forest_sequestration <- function(tree_density=NULL, avg_diameter=NULL, avg_height=NULL,
+                                      carbon_fraction = 0.5, bm_conversion_factor = 0.1, growth_rate = 0.03,tree_data=NULL){ 
   # Documentation:
-  #note: using metrics and decimals
+  # note: setting some values as NULL initially, in case there is a raw data table we use to compute those values first!
+  # > if not given the raw data table with necessary columns to compute these values, apply given values for each 
+  # note: using metrics and decimals
   # tree_density: number of trees per hectare
   # avg_diameter: average tree diameter (in cm)
   # avg_height: average tree height (in m)
@@ -11,7 +13,15 @@ forest_sequestration <- function(tree_density, avg_diameter, avg_height,
   # > placeholder value of 0.1 (midpoint for some studies of California trees - ranging from 0.05 to 0.15)
   # > would switch out depending on your data, and found using allometric modeling)
   # growth_rate: annual growth rate (as a decimal)
-  # > note: according to US Forest Service, it stands on avg at 3%. (0.03 in decimal)
+  # > note: according to US Forest Service (2021 report), it stands on avg at 3%. (0.03 in decimal)
+  
+  # If raw tree data is provided by user, calculate the averages and density.
+  if (!is.null(tree_data)) {
+    avg_diameter <- mean(tree_data$diameter, na.rm = TRUE)
+    avg_height   <- mean(tree_data$height, na.rm = TRUE)
+  
+  
+  
   
   
   # Combine tree dimensions into single variable for clarity / modularity
