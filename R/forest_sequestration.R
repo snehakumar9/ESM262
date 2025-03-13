ffa386d780d594010cf5a633d60b5a7ab2a67f46
bellaluvly <perezisabella11@gmail.com>
2025-03-13 09:05:25 -0700

    added some changes

--- R/forest_sequestration.R
+++ R/forest_sequestration.R
@@ -1,11 +1,11 @@
 forest_sequestration <- function(tree_density, avg_diameter, avg_height,
-                                      carbon_fraction = 0.5, bm_conversion_factor = 0.1, growth_rate = 0.03) {
+                                      carbon_fraction = 0.5, bm_conversion_factor = 0.1, growth_rate = 0.03){ 
   # Documentation:
   #note: using metrics and decimals
   # tree_density: number of trees per hectare
   # avg_diameter: average tree diameter (in cm)
   # avg_height: average tree height (in m)
-  # carbon_fraction: fraction of biomass that's carbon (on avg, 50% for diff tree parts)
+  # carbon_fraction: fraction of biomass that's carbon (on avg, 50% carbon composition for diff tree parts)
   # bm_conversion_factor: factor to convert tree dimensions (avg diameter and height) to biomass (in tons per tree)
   # > note: conversion factor varies / dependent on group of trees studied, not universal.
   # > placeholder value of 0.1 (midpoint for some studies of California trees - ranging from 0.05 to 0.15)
@@ -13,11 +13,8 @@ forest_sequestration <- function(tree_density, avg_diameter, avg_height,
   # growth_rate: annual growth rate (as a decimal)
   # > note: according to US Forest Service, it stands on avg at 3%. (0.03 in decimal)
   
-  # Calculate averages from the individual tree observations in data:
-  avg_diameter <- mean(tree_data$diameter, na.rm = TRUE)
-  avg_height   <- mean(tree_data$height, na.rm = TRUE)
   
-  # Combine tree dimensions into a single variable for clarity / modularity
+  # Combine tree dimensions into single variable for clarity / modularity
   tree_dimensions <- avg_diameter * avg_height
   
   # Estimate biomass per tree based on combined tree dimensions
@@ -31,8 +28,9 @@ forest_sequestration <- function(tree_density, avg_diameter, avg_height,
   
   # Return both outputs as a list
   return(list(annual_sequestration = annual_sequestration, total_biomass = total_biomass))
-}
 
-# Example usage:
+
+# Example usage (given some values):
 result2 <- forest_sequestration(tree_density = 500, avg_diameter = 30, avg_height = 10)
 print(result2)
+}
